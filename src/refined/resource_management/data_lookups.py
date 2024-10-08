c929d2145c8c7c430c4ffee7cfaa6979145a1416
jay-dhamale <90785542+jay-dhamale@users.noreply.github.com>
2024-10-08 10:17:35 +0530

    interim fixes

--- src/refined/resource_management/data_lookups.py
+++ src/refined/resource_management/data_lookups.py
@@ -79,7 +79,6 @@ class LookupsInferenceOnly:
         # can be shared
         self.tokenizers: PreTrainedTokenizer = AutoTokenizer.from_pretrained(
             os.path.dirname(resource_to_file_path["roberta_base_model"]),
-            add_special_tokens=False,
             add_prefix_space=False,
             use_fast=True,
         )
